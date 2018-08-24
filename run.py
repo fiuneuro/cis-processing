097132ae7a28f03de9c8700e1ec57992e7f80a62
Taylor Salo <tsalo006@fiu.edu>
2018-08-24 07:59:43 -0400

    Add informative error reporting.

--- run.py
+++ run.py
@@ -29,7 +29,9 @@ def run(command, env={}):
             break
 
     if process.returncode != 0:
-        raise Exception("Non zero return code: %d" % process.returncode)
+        raise Exception("Non zero return code: {0}\n"
+                        "{1}\n\n{2}".format(process.returncode, process.cmd,
+                                            process.output))
 
 
 def get_parser():
